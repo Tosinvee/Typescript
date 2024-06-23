b0443dea34838966f33cdfa0b5dc072c832a4661
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-06-23 14:49:30 +0100

    comment

--- classes.ts
+++ classes.ts
@@ -1,3 +1,4 @@
+/////ACCESS MODIFIERS IN TYPESCRIPT///////
 class Person {
     constructor(public name:string,  public age:number){
         this.name = name
@@ -14,3 +15,29 @@ class Person2 extends Person{
 const result = new Person2("shade", 19)
 result.getAge()
 console.log(result)
+
+//using protected
+class Proctected{
+    protected name: string
+    constructor(name: string){
+        this.name = name
+    }
+    public greet(){
+        console.log(`hello ${this.name}`)
+    }
+}
+
+class Proctected2 extends Proctected{
+    constructor(name:string){
+        super(name)
+    }
+    public greet(){
+        return `hello ${this.name}`
+    }
+    
+}
+
+const result2 = new Proctected2("shade")
+const result3 = new Proctected("tosin")
+console.log(result2.greet())
+console.log(result3.greet())
---
+++ generic.ts
@@ -0,0 +1,5 @@
+function identity<T>(arg:T):T {
+    return arg
+}
+let str = identity<string>("oluwatosin")
+    console.log(str)
