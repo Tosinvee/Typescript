d578488fdd775c32b1ce216f296bdfd6c49eeab7
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-06-23 15:25:55 +0100

    generic

--- generic.ts
+++ generic.ts
@@ -3,3 +3,10 @@ function identity<T>(arg:T):T {
 }
 let str = identity<string>("oluwatosin")
     console.log(str)
+
+function getProperty<T extends { name: string }>(obj: T, prop: keyof T): T[keyof T] {
+    return obj[prop];
+}
+
+let personn = { name: "shade" };
+console.log(getProperty(personn, "name")); // Output: shade
